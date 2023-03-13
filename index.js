bd0d250cbaca6d13340cd622fe3df7a9c5136c81
Ayush <ayushchauhan233@gmail.com>
2023-03-13 17:06:28 +0530

    test changes

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ let notes = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (req, res) => {
@@ -78,5 +78,5 @@ app.delete("/api/notes/:id", (request, response) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
